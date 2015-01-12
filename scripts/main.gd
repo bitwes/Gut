3841b4cc06bae328f0774f2cc14575fbec871c83
bitwes <butch.wesley@gmail.com>
2015-01-12 00:51:39 -0500

    some comments, moved call to gut tests so that the verify counts wouldn't fail.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,32 +1,44 @@
 extends Node2D
 
 func _ready():
-	#------------------------------------
-	#One line, print to console
-	#------------------------------------
+#------------------------------------
+#One line, print to console
+#------------------------------------
 	load('res://scripts/gut.gd').new().test_script('res://scripts/sample_tests.gd')
 
 	
-	#------------------------------------
-	#More lines, get result text out manually.  Can also inspect the results further 
-	#with a reference to the class.
-	#------------------------------------	
+#------------------------------------
+#More lines, get result text out manually.  Can also inspect the results further 
+#with a reference to the class.
+#------------------------------------	
 	#get an instance of gut
 	var tester = load('res://scripts/gut.gd').new()
 	add_child(tester)
+	
 	#stop it from printing to console
 	tester.set_should_print_to_console(false)
-	#change the log level to more detail
-	#tester.set_log_level(0)#tester.LOG_LEVEL_ALL_ASSERTS)
+	
+	#Run a single test script, this will not appear in the drop
+	#down in the display, but the first time it runs it will
+	#display the results.
+	tester.p("This is a one time script, notice it's not in the drop down")
 	tester.test_script('res://scripts/another_sample.gd')
-	#test the script
+	
+	#Add a bunch of test scripts to run.  These will appear in the drop
+	#down and can be rerun.  As long as you don't introduce a runtime
+	#error, you can leave it running, code some more, then rerun the
+	#tests for any or all of the scripts that have been added using
+	#add_script.
+	tester.add_script('res://scripts/gut_tests.gd')
 	tester.add_script('res://scripts/sample_tests.gd')
 	tester.add_script('res://scripts/another_sample.gd')
 	tester.add_script('res://scripts/all_passed.gd')
-	tester.add_script('res://scripts/gut_tests.gd')
 	tester.test_scripts()
-	#get the results out and send them to a text box
+
+	#get the results to the console, just to show you can get them
+	#out at the end of the process.
 	print(tester.get_result_text())
+	
 	#Insepect the results, put out some more text conditionally.
 	if(tester.get_fail_count() > 0):
 		tester.p("SOMEBODY BROKE SOMETHIN'!!\n")
