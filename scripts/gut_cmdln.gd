f7e32007d5eaa97f45d2ad3c93f13f92d2bad0a5
bitwes <butch.wesley@gmail.com>
2015-11-28 13:13:23 -0500

    refactor to make printing help easier

--- scripts/gut_cmdln.gd
+++ scripts/gut_cmdln.gd
@@ -39,6 +39,150 @@
 ################################################################################
 extends SceneTree
 
+class CmdLineParser:
+	var _opts = []
+	
+	func _init():
+		for i in range(OS.get_cmdline_args().size()):
+			_opts.append(OS.get_cmdline_args().get(i))
+			
+	# Search _opts for an element that starts with the option name
+	# specified.
+	func find_option(name):
+		var found = false
+		var idx = 0
+		
+		while(idx < _opts.size() and !found):
+			if(_opts[idx].find(name) == 0):
+				found = true
+			else:
+				idx += 1
+				
+		if(found):
+			return idx
+		else:
+			return -1
+
+	# Parse out the value of an option.  Values are seperated from
+	# the option name with "="
+	func get_option_value(full_option):
+		var split = full_option.split('=')
+	
+		if(split.size() > 1):
+			return split[1]
+		else:
+			return null
+	
+	# Parse out multiple comma delimited values from a command line
+	# option.  Values are separated from option name with "=" and 
+	# additional values are comma separated.
+	func get_option_array_value(full_option):
+		var value = get_option_value(full_option)
+		var split = value.split(',')
+		return split
+	
+	func get_array_value(option):
+		var to_return = []
+		var opt_loc = find_option(option)
+		if(opt_loc != -1):
+			to_return = get_option_array_value(_opts[opt_loc])
+			_opts.remove(opt_loc)
+	
+		return to_return
+	
+	# returns the value of an option if it was specfied, otherwise
+	# it returns the default.
+	func get_value(option, default):
+		var to_return = default
+		var opt_loc = find_option(option)
+		if(opt_loc != -1):
+			to_return = get_option_value(_opts[opt_loc])
+			_opts.remove(opt_loc)
+	
+		return to_return
+	
+	# returns true if it finds the option, false if not.
+	func was_specified(option):
+		var opt_loc = find_option(option)
+		if(opt_loc != -1):
+			_opts.remove(opt_loc)
+		
+		return opt_loc != -1	
+
+class Option:
+	var value = null
+	var option_name = ''
+	var default = null
+	var description = ''
+	
+	func _init(name, default_value, desc=''):
+		option_name = name
+		default = default_value
+		description = desc
+		value = default_value
+	
+	func to_s():
+		var subbed_desc = description
+		if(subbed_desc.find('[default]') != -1):
+			subbed_desc = subbed_desc.replace('[default]', default)
+		return option_name + "\t" + "\t" + subbed_desc
+		
+class Options:
+	var options = []
+	var _opts = []
+	var _banner = ''
+	
+	func add(name, default, desc):
+		options.append(Option.new(name, default, desc))
+	
+	func get_value(name):
+		var found = false
+		var idx = 0
+		
+		while(idx < options.size() and !found):
+			if(options[idx].option_name == name):
+				found = true
+			else:
+				idx += 1
+		
+		if(found):
+			return options[idx].value
+		else:
+			print("COULD NOT FIND OPTION " + name)
+			return null
+	
+	func set_banner(banner):
+		_banner = banner
+	
+	func print_help():
+		print(_banner)
+		
+		for i in range(options.size()):
+			print(options[i].to_s())
+		
+	func print_options():
+		for i in range(options.size()):
+			print(options[i].option_name + '=' + str(options[i].value))
+
+
+	func parse():
+		var parser = CmdLineParser.new()
+		
+		for i in range(options.size()):
+			var t = typeof(options[i].default)
+			if(t == TYPE_INT):
+				options[i].value = int(parser.get_value(options[i].option_name, options[i].default))
+			elif(t == TYPE_STRING):
+				options[i].value = parser.get_value(options[i].option_name, options[i].default)
+			elif(t == TYPE_ARRAY):
+				options[i].value = parser.get_array_value(options[i].option_name)
+			elif(t == TYPE_BOOL):
+				options[i].value = parser.was_specified(options[i].option_name)
+			elif(t == TYPE_NIL):
+				print(options[i].option_name + ' cannot be processed, it has a nil datatype')
+			else:
+				print(options[i].option_name + ' cannot be processsed, it has unknown datatype:' + str(t))
+				
 # instance of gut
 var _tester = null
 # array of command line options specified
@@ -53,9 +197,26 @@ var options = {
 	selected = '',
 	prefix = 'test_',
 	suffix = '.gd',
-	gut_location = 'res://scripts/gut.gd'
+	gut_location = 'res://scripts/gut.gd',
+	show_help = false
 }
 
+func fill_options():
+	var opts = Options.new()
+	opts.add('-gtest', [], 'Comma delimited list of tests to run')
+	opts.add('-gdir', [], 'Comma delimited list of directories to add tests from.')
+	opts.add('-gprefix', 'test_', 'Prefix used to find tests when specifying -gdir.  Default "[default]"')
+	opts.add('-gsuffix', '.gd', 'Suffix used to find tests when specifying -gdir.  Default "[default]"')
+	opts.add('-gexit', false, 'Exit after running tests.  If not specified you have to manually close the window.')
+	opts.add('-glog', 1, 'Log level.  Default [default]')
+	opts.add('-gignore_pause', false, 'Ignores any calls to gut.pause_before_teardown.')
+	opts.add('-gselect', '', ('Select a sccript to run initially.  The first script that ' + 
+	                          'was loaded using -gtest or -gdir that contains the specified ' + 
+	                          'string will be executed.  You may run others by interacting ' +
+                              'with the GUI.'))
+	opts.add('-gutloc', 'res://scripts/gut.gd', 'Full path (including name) of the gut script.  Default [default]')
+	return opts
+	
 # flag to say if we should run the scripts or not.  Is only
 # set to false if you specify a script to run with the -gselect
 # option and it cannot find the script.
@@ -141,23 +302,16 @@ func was_specified(option):
 	
 # Parses options, applying them to the _tester or setting values
 # in the options struct.
-func parse_options():
-	for i in range(OS.get_cmdline_args().size()):
-		_opts.append(OS.get_cmdline_args().get(i))
-
-	# add directories of tests
-	parse_option_dirs()
-	
-	# add tests
-	parse_option_tests()
-	
-	options.should_exit = was_specified('-gexit')
-	options.log_level = get_value('-glog', options.log_level)
-	options.ignore_pause_before_teardown = was_specified('-gignore_pause')
-	options.selected = get_value('-gselect', options.selected)
-	options.prefix = get_value('-gprefix', options.prefix)
-	options.suffix = get_value('-gsuffix', options.suffix)
-	options.gut_location = get_value('-gutloc', options.gut_location)
+func parse_options(opt):
+	options.tests = opt.get_value('-gtest')
+	options.dirs = opt.get_value('-gdir')
+	options.should_exit = opt.get_value('-gexit')
+	options.log_level = opt.get_value('-glog')
+	options.ignore_pause_before_teardown = opt.get_value('-gignore_pause')
+	options.selected = opt.get_value('-gselect')
+	options.prefix = opt.get_value('-gprefix')
+	options.suffix = opt.get_value('-gsuffix')
+	options.gut_location = opt.get_value('-gutloc')
 	
 	print(options)
 
@@ -197,8 +351,13 @@ func load_auto_load_scripts():
 
 # parse option and run Gut
 func _init():
+	var o = fill_options()
+	o.parse()
+	o.print_options()
+	parse_options(o)
+	
 	load_auto_load_scripts()
-	parse_options()
+	#parse_options()
 	apply_options()
 	
 	if(_auto_run):
