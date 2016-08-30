9ca40330bf35b2dd2e30b030fee661c7da6a8256
caryoscelus <caryoscelus@gmx.com>
2016-08-30 22:53:29 +0300

    return non-zero exit code on failing tests
    this is done in a hacky way currently, since Godot doesn't
    seem to support proper exit code specification. killing self
    process is used to achieve this instead

--- test/gut/gut_cmdln.gd
+++ test/gut/gut_cmdln.gd
@@ -339,4 +339,8 @@ func _init():
 # exit if option is set.
 func _on_tests_finished():
 	if(options.should_exit):
-		quit()
+		if(_tester._summary.failed>0):
+			# kill self => return non-zero exit code
+			OS.kill(OS.get_process_ID())
+		else:
+			quit()
