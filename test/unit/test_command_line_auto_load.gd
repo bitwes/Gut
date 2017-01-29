7114ee880febe9714e69865bb4a32c5d1115bb26
Butch Wesley <butch.wesley@gmail.com>
2017-01-28 20:42:41 -0500

    moved non-plugin files to GitTest repo

--- test/unit/test_command_line_auto_load.gd
+++
@@ -1,8 +0,0 @@
-#A sample script for illustrating multiple scripts and what it looks like
-#when all tests pass.
-extends "res://addons/gut/test.gd"
-
-func test_auto_load_works():
-  var g = get_node("/root/global")
-  g.print_loaded()
-  gut.assert_true("should get here")
