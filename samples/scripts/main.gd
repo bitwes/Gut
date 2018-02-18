aab137f7709b8e342eff358c9d41d0ec9e14895c
Butch Wesley <butch.wesley@gmail.com>
2018-02-18 00:34:52 -0500

    moved test directory out of samples

--- samples/scripts/main.gd
+++ samples/scripts/main.gd
@@ -52,51 +52,49 @@ func _run_test_one_line():
 	load('res://addons/gut/gut.gd').new().test_script('res://test/unit/sample_tests.gd')
 
 #------------------------------------
-# More lines, get result text out manually.  Can also inspect the results further 
+# More lines, get result text out manually.  Can also inspect the results further
 # with a reference to the class.
 #------------------------------------
 func _run_all_tests():
 	# get an instance of gut
 	tester = get_node("Gut")
-	
+
 	tester.connect('tests_finished', self, '_on_tests_finished')
 	tester.show()
 	tester.set_pos(Vector2(100, 100))
-	
+
 	tester.set_should_print_to_console(true)
-	
+
 	# Run a single test script, this will not appear in the drop
 	# down in the display, but the first time it runs it will
 	# display the results.
-	
-	tester.test_script('res://test_dir_load/test_samples3.gd')
+
+	tester.test_script('res://test/test_dir_load/test_samples3.gd')
 	tester.p("This is the results of running a single script.  Notice it's not in the drop down")
-	
+
 	# !! --------
 	# Set the yield between tests so that tests print as they complete
 	# instead of having to wait until the end.  It's a little slower,
 	# but you can tell what's going on.  Because it's slower it's
 	# disabled by default.
-	tester.set_yield_between_tests(true) 
+	tester.set_yield_between_tests(true)
 	# !! --------
-	
+
 	# Add all scripts in two directories.
 	tester.add_directory('res://test/unit')
 	tester.add_directory('res://test/integration')
-	
+
 	# Automatcially run all scripts when loaded.
 	tester.test_scripts()
-	
+
 	# Insepect the results, put out some more text conditionally.
 	if(tester.get_fail_count() > 0):
 		tester.p("SOMEBODY BROKE SOMETHIN'!!\n")
-	
+
 func _run_gut_tests():
 	tester = get_node("Gut")
-	
+
 	tester.set_should_print_to_console(false)
 	tester.add_script('res://test/unit/test_gut.gd')
 	tester.add_script('res://test/unit/test_gut_yielding.gd')
 	tester.set_yield_between_tests(true)
-	
-	
