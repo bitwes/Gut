b758db0355cfd3c3e2150bec032707a76857a617
Butch Wesley <butch.wesley@gmail.com>
2018-11-04 13:45:23 -0500

    fix #88

--- addons/gut/gut_cmdln.gd
+++ addons/gut/gut_cmdln.gd
@@ -465,8 +465,7 @@ func _init():
 	o.parse()
 	extract_command_line_options(o, opt_resolver.cmd_opts)
 	var load_result = \
-		load_options_from_config_file(options.config_file, opt_resolver.config_opts)
-
+			load_options_from_config_file(opt_resolver.get_value('config_file'), opt_resolver.config_opts)
 	if(load_result == -1): # -1 indicates json parse error
 		quit()
 	else:
