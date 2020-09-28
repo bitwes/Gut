acade52926b019970dbd7cba8514bf4159530025
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2020-09-28 19:55:20 +0200

    File paths for GDNative (#219)
    Special case when determining object filename of a GDNative script.
    Calling inst2dict fails for GDNative scripts,
    but we can directly obtain the resource path from the script instance.
    
    Fixes #211

--- addons/gut/strutils.gd
+++ addons/gut/strutils.gd
@@ -72,6 +72,10 @@ func _get_obj_filename(thing):
 			# If it isn't a packed scene and it doesn't have a script then
 			# we do nothing.  This just read better.
 			pass
+	elif(thing.get_script() is NativeScript):
+		# Work with GDNative scripts:
+		# inst2dict fails with "Not a script with an instance" on GDNative script instances
+		filename = _get_filename(thing.get_script().resource_path)
 	elif(!_utils.is_native_class(thing)):
 		var dict = inst2dict(thing)
 		filename = _get_filename(dict['@path'])
