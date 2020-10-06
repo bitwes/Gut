acf3a03c4a17fdbf7cccf51c65bb74d7aa902f79
Butch Wesley <butch.wesley@gmail.com>
2020-10-06 18:45:56 -0400

    implemented an array diff class.  Added quotes to type2str strings."

---
+++ addons/gut/array_diff.gd
@@ -0,0 +1,81 @@
+var _a1 = null
+var _a2 =  null
+var _size_diff_threshold = 10
+var strutils = load('res://addons/gut/utils.gd').get_instance().Strutils.new()
+
+var _different_indexes = null
+
+func _init(a1 = null, a2=null):
+	_a1 = a1
+	_a2 = a2
+
+
+func _do_datatypes_match(got, expected):
+	var got_type = typeof(got)
+	var expect_type = typeof(expected)
+	return !(got_type != expect_type and got != null and expected != null)
+
+
+func _populate_diff_indexes():
+	_different_indexes = []
+	for i in range(_a1.size()):
+		if(i < _a2.size()):
+			if(!_do_datatypes_match(_a1[i], _a2[i]) or _a1[i] != _a2[i]):
+					_different_indexes.append(i)
+		else:
+			_different_indexes.append(i)
+
+	if(_a1.size() < _a2.size()):
+		for i in range(_a1.size(), _a2.size()):
+			_different_indexes.append(i)
+
+func _make_diff_description(max_differences=20):
+	var to_return = ''
+	var limit = min(_different_indexes.size(), max_differences)
+
+	for i in range(limit):
+		var idx = _different_indexes[i]
+
+		var a1_str = '[missing]'
+		if(idx < _a1.size()):
+			a1_str = strutils.type2str(_a1[idx])
+
+		var a2_str = '[missing]'
+		if(idx < _a2.size()):
+			a2_str = strutils.type2str(_a2[idx])
+
+		to_return += str('  ', idx, ': ', a1_str, ' != ', a2_str, "\n")
+
+	if(_different_indexes.size() > limit):
+		to_return += str(limit, ' of  ', _different_indexes.size(), ' differences shown.')
+
+	return to_return
+
+func get_a1():
+	return _a1
+
+func set_a1(a1):
+	_a1 = a1
+
+func get_a2():
+	return _a2
+
+func set_a2(a2):
+	_a2 = a2
+
+func are_equal():
+	return _a1 == _a2
+
+func get_different_indexes():
+	_populate_diff_indexes()
+	return _different_indexes
+
+func summarize():
+	if(abs(_a1.size() - _a2.size()) > _size_diff_threshold):
+		return str('Arrays are different sizes:  a1(', _a1.size(), ') a2(', _a2.size(), ')')
+	_populate_diff_indexes()
+	var a1_str = strutils.truncate_string(str(_a1), 100)
+	var a2_str = strutils.truncate_string(str(_a2), 100)
+	var diff_str = _make_diff_description()
+
+	return str(a1_str, ' != ', a2_str, ".\nDifferent indexes = \n", diff_str)
---
+++ test/unit/test_array_diff.gd
@@ -0,0 +1,80 @@
+extends 'res://addons/gut/test.gd'
+
+var ArrayDiff = load('res://addons/gut/array_diff.gd')
+
+func test_can_make_one():
+	var ad = ArrayDiff.new()
+	assert_not_null(ad)
+
+func test_can_instantiate_with_two_arrays():
+	var ad  = ArrayDiff.new([], [])
+	assert_not_null(ad)
+
+func test_a1_accessors():
+	var ad  = ArrayDiff.new()
+	assert_accessors(ad, 'a1', null, [1, 2,  3])
+
+func test_a2_accessors():
+	var ad  = ArrayDiff.new()
+	assert_accessors(ad, 'a2', null, [1, 2,  3])
+
+func test_two_array_constructor_sets_a1_and_a2():
+	var a1 = [1, 2, 3]
+	var a2 = [3, 4, 5]
+	var ad = ArrayDiff.new(a1, a2)
+	assert_eq(ad.get_a1(), a1, 'a1')
+	assert_eq(ad.get_a2(), a2, 'a2')
+
+func test_is_equal_is_true_when_all_elements_match():
+	var ad = ArrayDiff.new([1, 2, 3], [1, 2, 3])
+	assert_true(ad.are_equal())
+
+func test_is_equal_returns_false_when_one_element_does_not_match():
+	var ad = ArrayDiff.new([1, 2, 3], [1, 2, 99])
+	assert_false(ad.are_equal(), 'should be false but is ' + str(ad.are_equal()))
+
+func test_can_get_list_of_different_indexes():
+	var ad = ArrayDiff.new([1, 2, 3], [3, 2, 1])
+	assert_eq(ad.get_different_indexes(), [0, 2])
+
+func test_get_different_indexes_works_when_a1_smaller():
+	var ad = ArrayDiff.new([1, 2, 3], [3, 2, 1, 98, 99])
+	assert_eq(ad.get_different_indexes(), [0, 2, 3 ,4])
+
+func test_get_different_indexes_works_when_a2_smaller():
+	var ad = ArrayDiff.new([3, 2, 1, 98, 99], [1, 2, 3])
+	assert_eq(ad.get_different_indexes(), [0, 2, 3, 4])
+
+func test_get_summary_text_lists_both_arrays():
+	var ad = ArrayDiff.new([3, 2, 1, 98, 99], [1, 2, 3])
+	assert_string_contains(ad.summarize(), '[3, 2, 1, 98, 99] != [1, 2, 3]')
+
+func test_get_summary_text_lists_differences():
+	var ad = ArrayDiff.new([3, 2, 1, 98, 99], [1, 2, 3])
+	assert_string_contains(ad.summarize(), '[0, 2, 3, 4]')
+
+func test_when_sizes_do_not_match_and_threshold_exceeded_then_summarize_tells_you():
+	var ad = ArrayDiff.new([3, 2, 1, 98, 99], [1, 2, 3])
+	ad._size_diff_threshold = 1
+	assert_string_contains(ad.summarize(), 'are different sizes')
+	assert_string_contains(ad.summarize(), '(5)', 'a1 size')
+	assert_string_contains(ad.summarize(), '(3)', 'a2 size')
+
+func test_when_arrays_are_large_then_summarize_truncates():
+	var a1 = []
+	var a2 = []
+	for i in range(100):
+		a1.append(i)
+		a2.append(i + 20)
+
+	var ad = ArrayDiff.new(a1, a2)
+	var summary = ad.summarize()
+	assert_lt(summary.length(), 500, summary)
+
+
+func test_works_with_strings_and_numbers():
+	var a1 = [0, 1, 2, 3, 4]
+	var a2 = [0, 'one', 'two', 'three', '4']
+	var ad = ArrayDiff.new(a1, a2)
+	gut.p(ad.summarize())
+	pass_test('we got here')
