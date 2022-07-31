8774cd05e02ac4bfe18a95bc26d9f41cc3750726
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-07-31 14:26:56 +0100

    :boom: delete native script

--- addons/gut/get_native_script.gd
+++
@@ -1,6 +0,0 @@
-# Since NativeExtension does not exist if GDExtension is not included in the build
-# of Godot this script is conditionally loaded only when NativeExtension exists.
-# You can then get a reference to NativeExtension for use in `is` checks by calling
-# get_it.
-static func get_it():
-	return NativeExtension
