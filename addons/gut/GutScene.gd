9319d8459b1d0ffb107d76a771ffe0ef0deb3fb2
Butch Wesley <butch.wesley@gmail.com>
2023-03-01 12:49:39 -0500

    draggable, resizeable, min/max, opacity

--- addons/gut/GutScene.gd
+++ addons/gut/GutScene.gd
@@ -15,10 +15,16 @@ class GuiHandler:
 		path_file = null,
 		prog_script = null,
 		prog_test = null,
+		resize_handle = null,
 		rtl = null,
 		rtl_bg = null,
-		time_label = null
+		switch_modes = null,
+		time_label = null,
+		title = null,
+		title_bar = null,
 	}
+
+	signal switch_modes()
 
 	func _init(gui_node):
 		_gui = gui_node
@@ -29,12 +35,21 @@ class GuiHandler:
 		_ctrls.path_file = _get_first_child_named('File', _gui)
 		_ctrls.prog_script = _get_first_child_named('ProgressScript', _gui)
 		_ctrls.prog_test = _get_first_child_named('ProgressTest', _gui)
+		_ctrls.resize_handle = _get_first_child_named("ResizeHandle", _gui)
 		_ctrls.rtl = _get_first_child_named('TestOutput', _gui)
 		_ctrls.rtl_bg = _get_first_child_named('OutputBG', _gui)
+		_ctrls.switch_modes = _get_first_child_named("SwitchModes", _gui)
 		_ctrls.time_label = _get_first_child_named('TimeLabel', _gui)
+		_ctrls.title = _get_first_child_named("Title", _gui)
+		_ctrls.title_bar = _get_first_child_named("TitleBar", _gui)
 
 		_ctrls.btn_continue.visible = false
 		_ctrls.btn_continue.pressed.connect(_on_continue_pressed)
+		_ctrls.switch_modes.pressed.connect(_on_switch_modes_pressed)
+		_ctrls.title_bar.gui_input.connect(_on_title_bar_input)
+		if(_ctrls.resize_handle != null):
+			_ctrls.resize_handle.gui_input.connect(_on_resize_handle_input)
+			_ctrls.resize_handle.draw.connect(_draw_resize_handle)
 
 		_ctrls.prog_script.value = 0
 		_ctrls.prog_test.value = 0
@@ -46,6 +61,43 @@ class GuiHandler:
 	# ------------------
 	# Events
 	# ------------------
+	var _title_mouse = {
+		down = false
+	}
+	func _on_title_bar_input(event : InputEvent):
+		if(event is InputEventMouseMotion):
+			if(_title_mouse.down):
+				_gui.position += event.relative
+		elif(event is InputEventMouseButton):
+			if(event.button_index == MOUSE_BUTTON_LEFT):
+				_title_mouse.down = event.pressed
+
+	var _resize_mouse = {
+		down = false
+	}
+	func _on_resize_handle_input(event : InputEvent):
+		if(event is InputEventMouseMotion):
+			if(_resize_mouse.down):
+				_gui.size += event.relative
+		elif(event is InputEventMouseButton):
+			if(event.button_index == MOUSE_BUTTON_LEFT):
+				_resize_mouse.down = event.pressed
+
+
+	func _draw_resize_handle(): # needs get_size()
+		# Draw the lines in the corner to show where you can
+		# drag to resize the dialog
+		var grab_margin = 2
+		var line_space = 4
+		var grab_line_color = Color(.4, .4, .4)
+		var rect_size = _ctrls.resize_handle.size
+		
+		for i in range(1, 10):
+			var x = rect_size - Vector2(i * line_space, grab_margin)
+			var y = rect_size - Vector2(grab_margin, i * line_space)
+			_ctrls.resize_handle.draw_line(x, y, grab_line_color, 1, true)
+
+
 	func _on_continue_pressed():
 		_ctrls.btn_continue.visible = false
 		_gut.end_teardown_pause()
@@ -82,9 +134,15 @@ class GuiHandler:
 
 	func _on_gut_end_pause():
 		pass
+
+	func _on_switch_modes_pressed():
+		switch_modes.emit()
+
 	# ------------------
 	# Private
 	# ------------------
+
+	
 	func _get_first_child_named(obj_name, parent_obj):
 		if(parent_obj == null):
 			return null
@@ -152,6 +210,23 @@ class GuiHandler:
 	func set_bg_color(c):
 		_ctrls.rtl_bg.color = c
 
+	func set_title(text):
+		_ctrls.title.text = text
+		
+	func to_top_left():
+		_gui.position = Vector2(5, 5)
+		
+	func to_bottom_right():
+		var win_size = DisplayServer.window_get_size()
+		_gui.position = win_size - Vector2i(_gui.size) - Vector2i(5, 5)
+		
+	func align_right():
+		var win_size = DisplayServer.window_get_size()
+		_gui.position.x = win_size.x - _gui.size.x -5
+		_gui.position.y = 5
+		_gui.size.y = win_size.y - 10
+		
+
 
 
 # ------------------------------------------------------------------------------
@@ -167,16 +242,30 @@ var gut = null :
 func _ready():
 	_large_handler = GuiHandler.new($Large)
 	_min_handler = GuiHandler.new($Min)
-	use_minimal_mode()
+
+	_large_handler.switch_modes.connect(use_minimal_mode.bind(true))
+	_min_handler.switch_modes.connect(use_minimal_mode.bind(false))
+
+	_large_handler.set_title("GUT")
+	_min_handler.set_title("GUT")
+	
+	_large_handler.align_right()
+	_min_handler.to_bottom_right()
+	
+	set_opacity(.5)
+	use_minimal_mode(false)
+
 
 func _process(_delta):
 	if(gut != null and gut.is_running()):
 		_large_handler.set_elapsed_time(gut.get_elapsed_time())
 		_min_handler.set_elapsed_time(gut.get_elapsed_time())
 
+
 func _set_gut(val):
 	_large_handler.set_gut(val)
 	_min_handler.set_gut(val)
+
 
 func get_textbox():
 	return _large_handler.get_textbox()
@@ -191,10 +280,11 @@ func set_font_size(new_size):
 		rtl.get('custom_fonts/italics_font').size = new_size
 		rtl.get('custom_fonts/normal_font').size = new_size
 
+
 func set_font(font_name):
 	_set_all_fonts_in_rtl(_large_handler.get_textbox(), font_name)
 
-# Needs rework for 4.0, DynamicFont DNE
+
 func _set_font(rtl, font_name, custom_name):
 	if(font_name == null):
 		rtl.add_theme_font_override(custom_name, null)
@@ -228,3 +318,8 @@ func set_background_color(color):
 func use_minimal_mode(should=true):
 	_min_handler.gui.visible = should
 	_large_handler.gui.visible = !should
+
+
+func set_opacity(val):
+	_large_handler.gui.modulate.a = val
+	_min_handler.gui.modulate.a = val
