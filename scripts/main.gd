7d902113747deb64c2fa0d9e67ada9ca2de0e82b
Butch Wesley <butch.wesley@gmail.com>
2023-03-18 13:02:55 -0400

    works, but causes 20 errors at start bc of _utils

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,118 +26,61 @@ extends Node2D
 # ##############################################################################
 
 # ##############################################################################
+#
 # Description:
 # ------------
-# This file is used to illustrate a couple ways of loading scripts and running
-# them.  This is also the method used to execute the tests of the Gut object
-# itself.
-#
-# The Gut object in the scene has already been configured through the editor
-# to load up various scripts.  You can experiment with the settings in the
-# scene or edit the code in this script.
+# This file is used to illustrate how you would Run tests on a deployed project
+# and some of the ways to interact with GUT, the runner, and a config.
+# 
 # ##############################################################################
+var GutConfig = load('res://addons/gut/gut_config.gd')
+var GutRunnerScene = load('res://addons/gut/gui/GutRunner.tscn')
 
-var tester = null
+var _config = GutConfig.new()
+var _gut_runner = GutRunnerScene.instantiate()
+
 
 func _ready():
-	# DO NOTHING HERE, connect to gut_ready instead.
-	pass
+	# Setup all the GUT options from a file.
+	_config.load_options('res://.gutconfig.json')
+	
+	# Override soecific values for the purposes of this 
+	# scene.  You can see all the options available in 
+	# the default_options dictionary in gut_config.gd
+	_config.options.selected = 'test_test.gd'
+	_config.options.should_exit = false
+	_config.options.compact_mode = false
 
-func _on_Gut_gut_ready():
-	#$Gut.export_if_tests_found()
-	#$Gut.import_tests_if_none_found()
+	# Connect to some of the GUT signals
+	var gut = _gut_runner.get_gut()
+	gut.start_run.connect(_on_gut_run_started)
+	gut.start_script.connect(_on_gut_start_script)
+	gut.start_test.connect(_on_gut_test_started)
+	gut.end_run.connect(_on_gut_run_ended)
 
-	# This line makes Gut use the export_path to load up the exported list
-	# of tests if it didn't find any tests.  This will occur after it has tried
-	# to load up all the configured directories.
-	#
-	# Using this line, you won't have to do anything to your test scene when
-	# running after being exported.
-	#
-	# You must have set export_path on the Gut object, exported your tests, and
-	# changed your project's export settings to include whatever file extension
-	# you gave your export file name.
-#	$Gut.import_tests_if_none_found()
+	# Stop tests from kicking off when the runner is "ready"
+	_gut_runner.auto_run_tests = false
+	# apply the config
+	_gut_runner.set_gut_config(_config)
+	add_child(_gut_runner)
+	
 
-	# -----
-	# Uncomment these lines to see various behaviors
-	# -----
-	#_run_test_one_line()
-	#_run_gut_tests(get_node('Gut'))
-	#_run_all_tests()
-	pass
-
-# Show that the signal is working.
-func _on_tests_finished():
-	tester.p("Tests done callback called")
-
-#------------------------------------
-# Example:
-# This creates an instance of Gut and runs a single script.  The output will
-# be visible in the console, not the Gut instance on the screen.
-#------------------------------------
-func _run_test_one_line():
-	load('res://addons/gut/gut.gd').new().test_script('res://test/samples/test_sample_all_passed.gd')
-
-#------------------------------------
-# Example:
-# More lines, get result text out manually.  Can also inspect the results further
-# with a reference to the class.
-#------------------------------------
-func _run_all_tests():
-	# get an instance of gut
-	tester = get_node("Gut")
-
-	tester.connect('tests_finished',Callable(self,'_on_tests_finished'))
-	tester.show()
-	tester.set_position(Vector2(100, 100))
-
-	tester.set_should_print_to_console(true)
-
-	# Add all scripts in two directories.
-	tester.add_directory('res://test/unit')
-	tester.add_directory('res://test/integration')
-
-	# Automatcially run all scripts when loaded.
-	tester.test_scripts(true)
-
-	# Insepect the results, put out some more text conditionally.
-	if(tester.get_fail_count() > 0):
-		tester.p("SOMEBODY BROKE SOMETHIN'!!\n")
-
-# These are all the tests that MUST be run to verify Gut is working as expected.
-# Some methods may include tests that are expected to fail.  Closely inspect
-# the resutls.
-func _run_gut_tests(gut):
-	gut.set_should_print_to_console(false)
-	gut.add_script('res://test/unit/test_doubler.gd')
-	gut.add_script('res://test/unit/test_gut_yielding.gd')
-	gut.add_script('res://test/unit/test_gut.gd')
-	gut.add_script('res://test/unit/test_signal_watcher.gd')
-	gut.add_script('res://test/unit/test_spy.gd')
-	gut.add_script('res://test/unit/test_stubber.gd')
-	gut.add_script('res://test/unit/test_summary.gd')
-	gut.add_script('res://test/unit/test_test_collector.gd')
-	gut.add_script('res://test/unit/test_test.gd')
-
-	gut.add_script('res://test/integration/test_test_stubber_doubler.gd')
-	gut.add_script('res://test/integration/test_doubler_and_stubber.gd')
-	gut.add_script('res://test/integration/test_gut_and_stubber.gd')
-	gut.add_script('res://test/integration/test_doubler_and_spy.gd')
-	gut.add_script('res://test/integration/test_gut_and_spy.gd')
-
-	# true says to run all the scripts, not just the first or
-	# the selected script.
-	gut.test_scripts()
-
-# Make a new Gut and run all the Gut specific tests.
 func _on_RunGutTestsButton_pressed():
-	var gut = load('res://addons/gut/gut.gd').new()
-	add_child(gut)
-	gut.set_position(Vector2(0, 0))
-	_run_gut_tests(gut)
-
-func _on_ExportTests_pressed():
-	$Gut.export_tests()
+	_gut_runner.run_tests()
 
 
+func _on_gut_run_started():
+	print('Gut Run Started')
+
+	
+func _on_gut_run_ended():
+	print('Gut Run Ended')
+
+
+# This signal passes a TestCollector.gd/TestScript instance
+func _on_gut_start_script(script_obj):
+	print(script_obj.get_full_name(), ' has ', script_obj.tests.size(), ' tests')
+
+
+func _on_gut_test_started(test_name):
+	print('  ', test_name)
