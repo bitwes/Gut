08cbd0da7a2e07c8d4f29f0ea0fb407979c4ad23
Butch Wesley <butch.wesley@gmail.com>
2024-06-10 10:40:41 -0400

    refactor arg parsing

--- addons/gut/optparse2.gd
+++ addons/gut/optparse2.gd
@@ -1,189 +1,19 @@
-# ##############################################################################
-#(G)odot (U)nit (T)est class
-#
-# ##############################################################################
-# The MIT License (MIT)
-# =====================
-#
-# Copyright (c) 2020 Tom "Butch" Wesley
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# ##############################################################################
-# ##############################################################################
-
-#-------------------------------------------------------------------------------
-# Performs basic command line argument parsing.  Once arguments have been parsed
-# use get_value, get_array_value, or was_specified to extract values.
-#-------------------------------------------------------------------------------
-class CmdLineParser:
-	var _used_options = []
-	# Holds the options that were specified and their values.  Positional
-	# arguments do not appear in this array.
-	# Example:
-	# --foo=bar one --bar asdf two three --hello four --world
-	# yields
-	# [[--foo, bar], [--bar, asdf], [--hello], [--world]]
-	var _opts = []
-	# After parsing, any values that are not an option or a value remain in
-	# positional_args in the order they appeared
-	# Example:
-	# 	--foo=bar one --bar asdf two three --hello four --world
-	# yields
-	#	[one, two, three]
-	var positional_args = []
-	var option_name_prefix = '-'
-
-
-	func _init(options):
-		_parse_command_line_arguments(options)
-
-
-	func _parse_command_line_arguments(args):
-		var parsed_opts = args.duplicate()
-		var i = 0
-		while i < parsed_opts.size():
-			var opt_val = [parsed_opts[i]]
-			var cur_val = parsed_opts[i]
-			if(is_option(cur_val)):
-				parsed_opts.remove_at(i)
-				if(cur_val.find('=') != -1):
-					opt_val = cur_val.split('=')
-				elif(i < parsed_opts.size() and !is_option(parsed_opts[i])):
-					opt_val =[cur_val, parsed_opts[i]]
-					parsed_opts.remove_at(i)
-				_opts.append(opt_val)
-			else:
-				positional_args.append(cur_val)
-				i += 1
-
-
-	func is_option(arg):
-		return arg.begins_with(option_name_prefix)
-
-
-	# Parse out multiple comma delimited values from a command line
-	# option.  Values are separated from option name with "=" and
-	# additional values are comma separated.
-	func _parse_array_value(full_option):
-		var value = _parse_option_value(full_option)
-		var split = value.split(',')
-		# This is what an empty set looks like from the command line.  If we do
-		# not do this then we will always get back [''] which is not what it
-		# shoudl be.
-		if(split.size() == 1 and split[0] == ''):
-			split = []
-		return split
-
-	# Parse out the value of an option.  Values are separated from
-	# the option name with "="
-	func _parse_option_value(full_option):
-		if(full_option.size() > 1):
-			return full_option[1]
-		else:
-			return null
-
-
-	# Search _opts for an element that starts with the option name
-	# specified.
-	func find_option(name):
-		var found = false
-		var idx = 0
-
-		while(idx < _opts.size() and !found):
-			if(_opts[idx][0] == name):
-				found = true
-			else:
-				idx += 1
-
-		if(found):
-			return idx
-		else:
-			return -1
-
-
-	func get_array_value(option):
-		_used_options.append(option)
-		var to_return = []
-		var opt_loc = find_option(option)
-		if(opt_loc != -1):
-			to_return = _parse_array_value(_opts[opt_loc])
-			_opts.remove_at(opt_loc)
-
-		return to_return
-
-
-	# returns the value of an option if it was specified, null otherwise.  This
-	# used to return the default but that became problemnatic when trying to
-	# punch through the different places where values could be specified.
-	func get_value(option, default=null):
-		_used_options.append(option)
-		var to_return = default
-		var opt_loc = find_option(option)
-		if(opt_loc != -1):
-			to_return = _parse_option_value(_opts[opt_loc])
-			_opts.remove_at(opt_loc)
-
-		return to_return
-
-
-	# returns true if it finds the option, false if not.
-	func was_specified(option):
-		_used_options.append(option)
-		return find_option(option) != -1
-
-
-	# Returns any unused command line options.  I found that only the -s and
-	# script name come through from godot, all other options that godot uses
-	# are not sent through OS.get_cmdline_args().
-	#
-	# This is a onetime thing b/c i kill all items in _used_options
-	func get_unused_options():
-		var to_return = []
-		for i in range(_opts.size()):
-			to_return.append(_opts[i][0])
-
-		var script_option = to_return.find("-s")
-		if script_option == -1:
-			script_option = to_return.find("--script")
-		if script_option != -1:
-			to_return.remove_at(script_option + 1)
-			to_return.remove_at(script_option)
-
-		while(_used_options.size() > 0):
-			var index = to_return.find(_used_options[0].split("=")[0])
-			if(index != -1):
-				to_return.remove_at(index)
-			_used_options.remove_at(0)
-
-		return to_return
-
-
-
-
 #-------------------------------------------------------------------------------
 # Simple class to hold a command line option
 #-------------------------------------------------------------------------------
 class Option:
 	static var empty_value = &'--__this_is_an_optparse_empty_value__--'
 
-	var value = empty_value
+	var _value = empty_value
+	var value = empty_value:
+		get:
+			if(str(_value) == empty_value):
+				return default
+			else:
+				return _value
+		set(val):
+			_value = val
+
 	var option_name = ''
 	var default = null
 	var description = ''
@@ -207,6 +37,9 @@ class Option:
 			subbed_desc = subbed_desc.replace('[default]', str(default))
 		return pad(option_name, min_space) + subbed_desc
 
+	func has_been_set():
+		return str(_value) != empty_value
+
 
 
 
@@ -216,27 +49,128 @@ class Option:
 # the command line and make it easily accessible.
 #-------------------------------------------------------------------------------
 var options = []
+var positional = []
 var banner = ''
+var option_name_prefix = '-'
 
-func add(name, default, desc):
-	options.append(Option.new(name, default, desc))
+
+func _convert_value_to_array(raw_value):
+	var split = raw_value.split(',')
+	# This is what an empty set looks like from the command line.  If we do
+	# not do this then we will always get back [''] which is not what it
+	# shoudl be.
+	if(split.size() == 1 and split[0] == ''):
+		split = []
+	return split
+
+
+# REMEMBER raw_value not used for bools.
+func _set_option_value(option, raw_value):
+	var t = typeof(option.default)
+	# only set values that were specified at the command line so that
+	# we can punch through default and config values correctly later.
+	# Without this check, you can't tell the difference between the
+	# defaults and what was specified, so you can't punch through
+	# higher level options.
+	if(t == TYPE_INT):
+		option.value = int(raw_value)
+	elif(t == TYPE_STRING):
+		option.value = str(raw_value)
+	elif(t == TYPE_ARRAY):
+		option.value = _convert_value_to_array(raw_value)
+	elif(t == TYPE_BOOL):
+		option.value = !option.default
+	elif(t == TYPE_FLOAT):
+		option.value = float(raw_value)
+	elif(t == TYPE_NIL):
+		print(option.option_name + ' cannot be processed, it has a nil datatype')
+	else:
+		print(option.option_name + ' cannot be processed, it has unknown datatype:' + str(t))
+
+
+func _is_option(arg):
+	return arg.begins_with(option_name_prefix)
+
+
+func _find_option_by_name(opt_name):
+	var idx = 0
+	var found_param = null
+	while(idx < options.size() and found_param == null):
+		if(options[idx].option_name == opt_name):
+			found_param = options[idx]
+		else:
+			idx += 1
+	return found_param
+
+
+func _parse_command_line_arguments(args):
+	var parsed_opts = args.duplicate()
+	var i = 0
+	var positional_index = 0
+
+	while i < parsed_opts.size():
+		var opt  = ''
+		var value = ''
+		var entry = parsed_opts[i]
+
+		if(_is_option(entry)):
+			parsed_opts.remove_at(i)
+
+			if(entry.find('=') != -1):
+				var parts = entry.split('=')
+				opt = parts[0]
+				value = parts[1]
+				var the_option = _find_option_by_name(opt)
+				if(the_option != null):
+					_set_option_value(the_option, value)
+			else:
+				var the_option = _find_option_by_name(entry)
+				if(the_option != null):
+					if(typeof(the_option.default) == TYPE_BOOL):
+						_set_option_value(the_option, null)
+					elif(i < parsed_opts.size() and !_is_option(parsed_opts[i])):
+						value = parsed_opts[i]
+						parsed_opts.remove_at(i)
+						_set_option_value(the_option, value)
+		else:
+			if(positional_index < positional.size()):
+				_set_option_value(positional[positional_index], entry)
+				parsed_opts.remove_at(i)
+				positional_index += 1
+			else:
+				i += 1
+
+	return parsed_opts
+
+
+func add(op_name, default, desc):
+	options.append(Option.new(op_name, default, desc))
+
+
+func add_positional(op_name, default, desc):
+	positional.append(Option.new(op_name, default, desc))
 
 
 func get_value(name):
-	var found = false
 	var idx = 0
+	var found_param = null
 
-	while(idx < options.size() and !found):
+	while(idx < options.size() and found_param == null):
 		if(options[idx].option_name == name):
-			found = true
+			found_param = options[idx]
 		else:
 			idx += 1
 
-	if(found):
-		var val = options[idx].value
-		if(val == Option.empty_value):
-			val = options[idx].default
-		return val
+	if(found_param == null):
+		idx = 0
+		while(idx < positional.size() and found_param == null):
+			if(positional[idx].option_name == name):
+				found_param = positional[idx]
+			else:
+				idx += 1
+
+	if(found_param != null):
+		return found_param.value
 	else:
 		print("COULD NOT FIND OPTION " + name)
 		return null
@@ -267,34 +201,69 @@ func print_options():
 
 
 func parse(cli_args=OS.get_cmdline_args()):
-	var parser = CmdLineParser.new(cli_args)
+	var unused = _parse_command_line_arguments(cli_args)
 
-	for i in range(options.size()):
-		var t = typeof(options[i].default)
-		# only set values that were specified at the command line so that
-		# we can punch through default and config values correctly later.
-		# Without this check, you can't tell the difference between the
-		# defaults and what was specified, so you can't punch through
-		# higher level options.
-		if(parser.was_specified(options[i].option_name)):
-			if(t == TYPE_INT):
-				options[i].value = int(parser.get_value(options[i].option_name))
-			elif(t == TYPE_STRING):
-				options[i].value = parser.get_value(options[i].option_name)
-			elif(t == TYPE_ARRAY):
-				options[i].value = parser.get_array_value(options[i].option_name)
-			elif(t == TYPE_BOOL):
-				options[i].value = parser.was_specified(options[i].option_name)
-			elif(t == TYPE_FLOAT):
-				options[i].value = parser.get_value(options[i].option_name)
-			elif(t == TYPE_NIL):
-				print(options[i].option_name + ' cannot be processed, it has a nil datatype')
-			else:
-				print(options[i].option_name + ' cannot be processed, it has unknown datatype:' + str(t))
-
-	var unused = parser.get_unused_options()
 	if(unused.size() > 0):
 		print("Unrecognized options:  ", unused)
 		return false
 
 	return true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ##############################################################################
+# The MIT License (MIT)
+# =====================
+#
+# Copyright (c) 2024 Tom "Butch" Wesley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ##############################################################################
--- test/unit/test_optparse.gd
+++ test/unit/test_optparse.gd
@@ -1,79 +1,41 @@
 extends GutTest
 
-
-
-class TestCmdLineParser:
+class TestOption:
 	extends GutTest
 
 	var OptParse = load('res://addons/gut/optparse2.gd')
 
 	func test_can_make_one():
-		var cli_p = OptParse.CmdLineParser.new([])
-		assert_not_null(cli_p)
+		var o = OptParse.Option.new('name', 'default')
+		assert_not_null(o)
 
-	func test_was_specified_false_for_non_specified_options():
-		var cli_p = OptParse.CmdLineParser.new([])
-		assert_false(cli_p.was_specified('--foo'))
+	func test_init_sets_values():
+		var o = OptParse.Option.new('name', 'default', 'description')
+		assert_eq(o.option_name, 'name')
+		assert_eq(o.default, 'default')
+		assert_eq(o.description, 'description')
 
-	func test_was_specified_true_for_specified_options():
-		var cli_p = OptParse.CmdLineParser.new(['--foo'])
-		assert_true(cli_p.was_specified('--foo'))
+	func test_has_been_set_false_by_default():
+		var o = OptParse.Option.new('name', 'default')
+		assert_false(o.has_been_set())
 
-	func test_get_value_null_by_default():
-		var cli_p = OptParse.CmdLineParser.new(['--foo'])
-		assert_null(cli_p.get_value('--foo'))
+	func test_has_been_set_true_after_setting_value():
+		var o = OptParse.Option.new('name', 'default')
+		o.value = 'value'
+		assert_true(o.has_been_set())
 
-	func test_get_value_returns_default_when_option_not_specified():
-		var cli_p = OptParse.CmdLineParser.new(['one'])
-		assert_eq(cli_p.get_value('--foo', 'default'), 'default')
+	func test_value_returns_default_when_value_not_set():
+		var o = OptParse.Option.new('name', 'default')
+		assert_eq(o.value, 'default')
 
-	func test_splits_value_on_equal_sign():
-		var cli_p = OptParse.CmdLineParser.new(['--foo=bar'])
-		assert_eq(cli_p.get_value('--foo'), 'bar')
+	func test_value_returned_when_value_has_been_set():
+		var o = OptParse.Option.new('name', 'default')
+		o.value = 'value'
+		assert_eq(o.value, 'value')
 
-	func test_sets_value_when_next_element_when_is_not_an_option():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'bar'])
-		assert_eq(cli_p.get_value('--foo'), 'bar')
-
-	func test_does_not_set_value_when_next_element_when_is_an_option():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', '--bar'])
-		assert_null(cli_p.get_value('--foo'))
-
-	func test_non_option_args_are_in_poisitional_args():
-		var cli_p = OptParse.CmdLineParser.new(['one', 'two', 'three'])
-		assert_eq(cli_p.positional_args, ['one', 'two', 'three'])
-
-	func test_positional_arguments_appear_in_order_they_were_specified_minus_other_args_and_values():
-		var cli_p = OptParse.CmdLineParser.new(
-			["--foo=bar", "one", "--bar", "asdf", "two", "three", "--hello", "--world"])
-		assert_eq(cli_p.positional_args, ['one', 'two', 'three'])
-
-	func test_get_array_value_parses_commas_when_equal_not_used():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d'])
-		assert_eq(cli_p.get_array_value('--foo'), PackedStringArray(['a', 'b', 'c', 'd']))
-
-	func test_all_options_are_unused_by_default():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
-		assert_eq(cli_p.get_unused_options().size(), 3)
-
-	func test_was_specified_removes_opt_from_unused_opts():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
-		cli_p.was_specified('--bar')
-		var unused = cli_p.get_unused_options()
-		assert_ne(unused[1], '--bar')
-
-	func test_get_array_value_removes_opt_from_unused_opts():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
-		cli_p.get_array_value('--foo')
-		var unused = cli_p.get_unused_options()
-		assert_ne(unused[0], '--foo')
-
-	func test_get_value_removes_opt_from_unused_opts():
-		var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
-		cli_p.get_value('--foo')
-		var unused = cli_p.get_unused_options()
-		assert_ne(unused[0], '--foo')
-
+	func test_to_s_replaces_default_with_default_value():
+		var o = OptParse.Option.new('name', 'foobar', 'put default here [default]')
+		assert_string_ends_with(o.to_s(), 'put default here foobar')
 
 
 
@@ -120,5 +82,151 @@ class TestOptParse:
 		var help = opts.get_help()
 		assert_string_contains(help, "foo = bar")
 		assert_string_contains(help, "bar = foo")
+
+
+
+
+
+	# func test_get_value_null_by_default():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo'])
+	# 	assert_null(cli_p.get_value('--foo'))
+
+	# func test_get_value_returns_default_when_option_not_specified():
+	# 	var cli_p = OptParse.CmdLineParser.new(['one'])
+	# 	assert_eq(cli_p.get_value('--foo', 'default'), 'default')
+
+	# func test_splits_value_on_equal_sign():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo=bar'])
+	# 	assert_eq(cli_p.get_value('--foo'), 'bar')
+
+	# func test_sets_value_when_next_element_when_is_not_an_option():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', 'bar'])
+	# 	assert_eq(cli_p.get_value('--foo'), 'bar')
+
+	# func test_does_not_set_value_when_next_element_when_is_an_option():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', '--bar'])
+	# 	assert_null(cli_p.get_value('--foo'))
+
+
+	# func test_get_array_value_removes_opt_from_unused_opts():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
+	# 	cli_p.get_array_value('--foo')
+	# 	var unused = cli_p.get_unused_options()
+	# 	assert_ne(unused[0], '--foo')
+
+	# func test_get_value_removes_opt_from_unused_opts():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
+	# 	cli_p.get_value('--foo')
+	# 	var unused = cli_p.get_unused_options()
+	# 	assert_ne(unused[0], '--foo')
+
+	# func test_positional_arguments_appear_in_order_they_were_specified_minus_other_args_and_values():
+	# 	var cli_p = OptParse.CmdLineParser.new(
+	# 		["--foo=bar", "one", "--bar", "asdf", "two", "three", "--hello", "--world"])
+	# 	assert_eq(cli_p.positional_args, ['one', 'two', 'three'])
+
+	# func test_all_options_are_unused_by_default():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
+	# 	assert_eq(cli_p.get_unused_options().size(), 3)
+
+	# func test_was_specified_removes_opt_from_unused_opts():
+	# 	var cli_p = OptParse.CmdLineParser.new(['--foo', 'a,b,c,d', '--bar', '--asdf'])
+	# 	cli_p.was_specified('--bar')
+	# 	var unused = cli_p.get_unused_options()
+	# 	assert_ne(unused[1], '--bar')
+
+
+class TestBooleanValues:
+	extends GutTest
+
+	var OptParse = load('res://addons/gut/optparse2.gd')
+
+	func test_gets_default_of_false_when_not_specified():
+		var op = OptParse.new()
+		op.add('--foo', false, 'foo bar')
+		op.parse([])
+		assert_false(op.get_value('--foo'))
+
+	func test_gets_default_of_true_when_not_specified():
+		var op = OptParse.new()
+		op.add('--foo', true, 'foo bar')
+		op.parse([])
+		assert_true(op.get_value('--foo'))
+
+	func test_is_true_when_specified_and_default_false():
+		var op = OptParse.new()
+		op.add('--foo', false, 'foo bar')
+		op.parse(['--foo'])
+		assert_true(op.get_value('--foo'))
+
+	func test_is_false_when_specified_and_default_true():
+		var op = OptParse.new()
+		op.add('--foo', true, 'foo bar')
+		op.parse(['--foo'])
+		assert_false(op.get_value('--foo'))
+
+	func test_does_not_get_value_of_unnamed_args_after():
+		var op = OptParse.new()
+		op.add('--foo', false, 'foo bar')
+		op.parse(['--foo', 'asdf'])
+		assert_true(op.get_value('--foo'))
+
+
+
+class TestArrayParameters:
+	extends GutTest
+
+	var OptParse = load('res://addons/gut/optparse2.gd')
+
+
+	func test_get_array_value_parses_commas_when_equal_not_used():
+		var op = OptParse.new()
+		op.add('--foo', [], 'foo array')
+		op.parse(['--foo', 'a,b,c,d'])
+		assert_eq(op.get_value('--foo'), PackedStringArray(['a', 'b', 'c', 'd']))
+
+
+
+
+class TestPositionalArguments:
+	extends GutTest
+
+	var OptParse = load('res://addons/gut/optparse2.gd')
+
+	func test_can_add_positional_argument():
+		var op = OptParse.new()
+		op.add_positional('first', '', 'the first one')
+		assert_eq(op.positional.size(), 1)
+
+	func test_non_named_parameter_1_goes_into_positional():
+		var op = OptParse.new()
+		op.add_positional('first', '', 'the first one')
+		op.parse(['this is a value'])
+		assert_eq(op.get_value('first'), 'this is a value')
+
+	func test_two_positional_parameters():
+		var op = OptParse.new()
+		op.add_positional('first', '', 'the first one')
+		op.add_positional('second', 'not_set', 'the second one')
+		op.parse(['foo', 'bar'])
+		assert_eq(op.get_value('first'), 'foo')
+		assert_eq(op.get_value('second'), 'bar')
+
+	func test_second_positional_gets_default_when_not_set():
+		var op = OptParse.new()
+		op.add_positional('first', '', 'the first one')
+		op.add_positional('second', 'not_set', 'the second one')
+		op.parse(['foo'])
+		assert_eq(op.get_value('first'), 'foo')
+		assert_eq(op.get_value('second'), 'not_set')
+
+	func test_when_preceeding_parameter_is_bool_positional_gets_set():
+		var op = OptParse.new()
+		op.add('--bool', false, 'this is a bool')
+		op.add_positional('first', '', 'the first one')
+		op.parse(['--bool', 'foo'])
+		assert_eq(op.get_value('first'), 'foo')
+		assert_true(op.get_value('--bool'))
+
 
 
