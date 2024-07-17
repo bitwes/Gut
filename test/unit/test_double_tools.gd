ebdd041a13baad70e5ec76556ad8d58cf12209c3
Butch Wesley <butch.wesley@gmail.com>
2024-07-17 01:21:04 -0400

    refactor

--- test/unit/test_double_tools.gd
+++ test/unit/test_double_tools.gd
@@ -10,6 +10,9 @@ class FakeBase:
 	func set_foo(val):
 		foo = val
 
+	func get_foo():
+		return foo
+
 
 class FakeDouble:
 	extends FakeBase
@@ -37,10 +40,10 @@ func test_get_method_to_call_returns_null_when_not_stubbed():
 	dt.stubber = stbr
 	dt.double = fd
 
-	assert_null(dt.get_method_to_call('set_foo', [1]))
+	assert_null(dt.handle_other_stubs('set_foo', [1]))
 
 
-func test_get_method_to_call_returns_the_stubbed_method():
+func test_handle_other_stubs_calls_method():
 	var fd = FakeDouble.new()
 
 	var sp = StubParams.new(fd.set_foo)
@@ -53,27 +56,42 @@ func test_get_method_to_call_returns_the_stubbed_method():
 	dt.stubber = stbr
 	dt.double = fd
 
-	var result = dt.get_method_to_call('set_foo', [1])
-	assert_eq(result.get_object(), self, "object")
-	assert_eq(result.get_method(), "call_this", "method")
 
-
-func test_calling_the_method_uses_parameters_passed():
-	var fd = FakeDouble.new()
-
-	var sp = StubParams.new(fd.set_foo)
-	sp.to_call(call_this)
-
-	var stbr = Stubber.new()
-	stbr.add_stub(sp)
-
-	var dt = DoubleTools.new()
-	dt.stubber = stbr
-	dt.double = fd
-
-	var result = dt.get_method_to_call('set_foo', [1])
-	result.call()
+	dt.handle_other_stubs('set_foo', [1])
 	assert_eq(call_this_was_called, 1)
+
+func test_handle_other_stubs_returns_value():
+	var fd = FakeDouble.new()
+
+	var sp = StubParams.new(fd.get_foo)
+	sp.to_call(func(): return 'asdf')
+
+	var stbr = Stubber.new()
+	stbr.add_stub(sp)
+
+	var dt = DoubleTools.new()
+	dt.stubber = stbr
+	dt.double = fd
+
+	var result = dt.handle_other_stubs('get_foo', [])
+	assert_eq(result, 'asdf')
+
+# func test_calling_the_method_uses_parameters_passed():
+# 	var fd = FakeDouble.new()
+
+# 	var sp = StubParams.new(fd.set_foo)
+# 	sp.to_call(call_this)
+
+# 	var stbr = Stubber.new()
+# 	stbr.add_stub(sp)
+
+# 	var dt = DoubleTools.new()
+# 	dt.stubber = stbr
+# 	dt.double = fd
+
+# 	var result = dt.handle_other_stubs('set_foo', [1])
+# 	result.call()
+# 	assert_eq(call_this_was_called, 1)
 
 
 # func test_print_source():
