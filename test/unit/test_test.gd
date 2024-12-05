cfa5f4424d16fd70525f9dab31c7313cb99164d1
Omar - Home <negmok@gmail.com>
2024-12-05 13:25:28 -0500

    Revert "Add failing test for vector4s"
    This reverts commit 69cdfb89dc213cf99bd6c2375ec04b3b26eeab77.

--- test/unit/test_test.gd
+++ test/unit/test_test.gd
@@ -301,16 +301,12 @@ class TestAssertAlmostEq:
 		gr.test.assert_almost_eq(Vector3(1.0, 2.0, 3.0), Vector3(1.0, 1.0, 1.0), Vector3(2.0, 2.0, 2.0), "Should pass, Vector3(1.0, 2.0, 3.0) == Vector3(1.0, 1.0, 1.0) +/- Vector3(2.0, 2.0, 2.0)")
 		assert_pass(gr.test)
 		
-	func test_passes_with_vector4s_within_range():
+	func test_passes_with_vecor4s_within_range():
 		gr.test.assert_almost_eq(Vector4(1.0, 2.0, 3.0, 1.0), Vector4(1.0, 1.0, 1.0, 1.0), Vector4(2.0, 2.0, 2.0, 2.0), "Should pass")
 		assert_pass(gr.test)
 		
 	func test_fails_with_vector4_outside_range():
 		gr.test.assert_almost_eq(Vector4(1.0, 2.0, 3.0, 1.0), Vector4(9.0, 1.0, 1.0, 1.0), Vector4(2.0, 2.0, 2.0, 2.0), "Should fail")
-		assert_fail(gr.test)
-		
-	func test_fails_when_vector4_y_outside_range():
-		gr.test.assert_almost_eq(Vector4(1.0, 2.0, 99.0, 1.0), Vector4(1.0, 1.0, 1.0, 1.0), Vector4(2.0, 2.0, 2.0, 2.0), "Should fail")
 		assert_fail(gr.test)
 
 	func test_fail_message_includes_extra_precision_for_floats():
