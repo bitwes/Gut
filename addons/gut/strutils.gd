336187949d0473bebd3d5b2225a366244c6d72b4
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-30 15:34:38 -0700

    Added docstrings and type hints to addons/gut/junit_xml_export and one or two to addons/gut/strutils

--- addons/gut/strutils.gd
+++ addons/gut/strutils.gd
@@ -153,25 +153,22 @@ func truncate_string(src, max_size):
 	return to_return
 
 
-func _get_indent_text(times, pad):
-	var to_return = ''
-	for i in range(times):
-		to_return += pad
-
-	return to_return
-
-func indent_text(text, times, pad):
+## Indents multiline text. Indents every line of [param text] with [param times]
+## copies of the String [param pad]. If the string ends with a newline,
+## no indent is added after that newline character and the resulting string
+## will still end on a newline.
+func indent_text(text: String, times: int, pad: String) -> String:
 	if(times == 0):
 		return text
 
-	var to_return = text
-	var ending_newline = ''
+	var to_return := text
+	var ending_newline := ''
 
 	if(text.ends_with("\n")):
 		ending_newline = "\n"
-		to_return = to_return.left(to_return.length() -1)
+		to_return = to_return.left(-1)
 
-	var padding = _get_indent_text(times, pad)
+	var padding := pad.repeat(times)
 	to_return = to_return.replace("\n", "\n" + padding)
 	to_return += ending_newline
 
