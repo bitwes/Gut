499e7e01ee9570c312744099533337ca804d559a
Butch Wesley <butch.wesley@gmail.com>
2025-07-10 18:35:43 -0400

    more test/illustration of vararg issue

--- test/unit/test_bugs/test_4_5_issues.gd
+++ test/unit/test_bugs/test_4_5_issues.gd
@@ -1,6 +1,14 @@
 extends GutTest
 
 var Doubler = load('res://addons/gut/doubler.gd')
+var MethodMaker = load("res://addons/gut/method_maker.gd")
+
+# class MyNode:
+# 	extends Node
+
+# 	func rpc(s1: StringName ='', p2='', p3='', p4='')-> Error:
+# 		pass
+
 
 func test_can_make_a_double_of_node():
 	var doubler = Doubler.new()
@@ -16,3 +24,14 @@ func test_this_fails_until_varargs_are_included():
 	var inst = d.new()
 	assert_ne(inst.__gutdbl_values.doubled_methods.find("rpc"), -1, 'Found rpc in list of doubled method')
 	assert_ne(inst.__gutdbl_values.doubled_methods.find("call_deferred"), -1, "found call_deferred in list of doubled methods")
+
+
+func test_see_what_we_can_do():
+	var mm = MethodMaker.new()
+	var inst = Node.new()
+	var meta = []
+	for entry in inst.get_method_list():
+		if(entry.name == "rpc"):
+			meta = entry
+	var text = mm.get_function_text(meta)
+	print(text)
