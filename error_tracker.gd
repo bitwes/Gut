44da07f4c7f711a68de4ca3a5230f119fa779e88
Butch Wesley <butch.wesley@gmail.com>
2025-07-22 15:33:25 -0400

    error tracker complete

--- error_tracker.gd
+++ error_tracker.gd
@@ -4,17 +4,9 @@ class_name GutErrorTracker
 const NO_TEST := 'NONE'
 const GUT_ERROR_TYPE = 999
 
-enum GUT_ERRORS_CAUSE {
+enum TREAT_AS {
 	NOTHING,
 	FAILURE,
-	RISKY
-}
-
-enum ERROR_CATEGORY {
-	ENGINE ,
-	ASSERT,
-	PUSH_ERROR,
-	GUT,
 }
 
 
@@ -43,10 +35,6 @@ class TrackedError:
 	func is_push_error():
 		return error_type != GUT_ERROR_TYPE and function == "push_error"
 
-	# this might not work in other languages.
-	func is_assert():
-		return error_type == Logger.ERROR_TYPE_SCRIPT and code.find("Assertion failed.") == 0
-
 
 	func is_engine_error():
 		return error_type != GUT_ERROR_TYPE
@@ -56,28 +44,31 @@ class TrackedError:
 		return error_type == GUT_ERROR_TYPE
 
 
-	func get_error_category() -> ERROR_CATEGORY:
-		return ERROR_CATEGORY.GUT
+	# this might not work in other languages, and is flakey at best.
+	# func is_assert():
+	# 	return error_type == Logger.ERROR_TYPE_SCRIPT and \
+	# 		(code.find("Assertion failed.") == 0 or \
+	# 			code.find("Assertion failed:") == 0)
+
 
 
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 var _current_test_id = NO_TEST
-var _error_count = 0
 var _mutex = Mutex.new()
 
-var gut = null
-var logger = GutUtils.get_logger()
 var errors = GutUtils.OneToMany.new()
-var treat_errors_as : GUT_ERRORS_CAUSE = GUT_ERRORS_CAUSE.NOTHING
-var treat_asserts_as : GUT_ERRORS_CAUSE = GUT_ERRORS_CAUSE.NOTHING
+
+var treat_gut_errors_as : TREAT_AS = TREAT_AS.NOTHING
+var treat_engine_errors_as : TREAT_AS = TREAT_AS.NOTHING
+var treat_push_error_as : TREAT_AS = TREAT_AS.NOTHING
 
 
 func _get_stack_data(current_test_name):
 	var test_entry = {}
-	# if stack trace available than extraxt the test case line number
 	var stackTrace = get_stack()
+
 	if(stackTrace!=null):
 		var index = 0
 		while(index < stackTrace.size() and test_entry == {}):
@@ -97,6 +88,32 @@ func _get_stack_data(current_test_name):
 	}
 
 
+func _is_error_failable(error : TrackedError):
+	var is_it = false
+	if(error.is_gut_error()):
+		if(treat_gut_errors_as == TREAT_AS.FAILURE):
+			is_it = true
+	elif(error.is_push_error()):
+		if(treat_push_error_as == TREAT_AS.FAILURE):
+			is_it = true
+	elif(error.is_engine_error()):
+		if(treat_engine_errors_as == TREAT_AS.FAILURE):
+			is_it = true
+	return is_it
+
+
+# Godot's Logger virtual method for errors
+func _log_error(function: String, file: String, line: int,
+	code: String, rationale: String, editor_notify: bool,
+	error_type: int, script_backtraces: Array[ScriptBacktrace]) -> void:
+
+		add_error(function, file, line,
+			code, rationale, editor_notify,
+			error_type, script_backtraces)
+
+# Godot's Logger virtual method for any output?
+# func _log_message(message: String, error: bool) -> void:
+# 	pass
 
 
 func start_test(test_id):
@@ -113,14 +130,35 @@ func did_test_error(test_id=_current_test_id):
 
 # This should look through all the errors for a test and see if a failure
 # should happen based off of flags.
-func should_test_fail_from_error(test_id = _current_test_id):
-	return false
+func should_test_fail_from_errors(test_id = _current_test_id):
+	var to_return = false
+	if(errors.items.has(test_id)):
+		var errs = errors.items[test_id]
+		var index = 0
+		while(index < errs.size() and !to_return):
+			var error = errs[index]
+			to_return = _is_error_failable(error)
+			index += 1
+	return to_return
 
 
-# Returns null or a string for errors that occurred during the test that should
-# cause failure based on this class' flags.
-func get_fail_text_for_errors(test_id=_current_test_id):
-	return null
+# Returns emtpy string or text for errors that occurred during the test that
+# should cause failure based on this class' flags.
+func get_fail_text_for_errors(test_id=_current_test_id) -> String:
+	var error_texts = []
+
+	if(errors.items.has(test_id)):
+		for error in errors.items[test_id]:
+			if(_is_error_failable(error)):
+				error_texts.append(error.code)
+
+	var to_return = ""
+	for i in error_texts.size():
+		if(to_return != ""):
+			to_return += "\n"
+		to_return += str("[", i + 1, "] ", error_texts[i])
+
+	return to_return
 
 
 func add_gut_error(text) -> TrackedError:
@@ -142,7 +180,6 @@ func add_error(function: String, file: String, line: int,
 
 		_mutex.lock()
 
-		_error_count += 1
 		var err := TrackedError.new()
 
 		err.backtrace = script_backtraces
@@ -160,16 +197,3 @@ func add_error(function: String, file: String, line: int,
 		_mutex.unlock()
 
 		return err
-
-
-# Godot's Logger virtual method for errors
-func _log_error(function: String, file: String, line: int,
-	code: String, rationale: String, editor_notify: bool,
-	error_type: int, script_backtraces: Array[ScriptBacktrace]) -> void:
-
-		add_error(function, file, line,
-			code, rationale, editor_notify,
-			error_type, script_backtraces)
-
-# func _log_message(message: String, error: bool) -> void:
-# 	pass
--- test/unit/test_error_tracker.gd
+++ test/unit/test_error_tracker.gd
@@ -1,29 +1,29 @@
 extends GutTest
 
-var _added_tracker = GutErrorTracker.new()
-
-func before_all():
-	OS.add_logger(_added_tracker)
+var _added_tracker
 
 
 func before_each():
+	_added_tracker = GutErrorTracker.new()
+	OS.add_logger(_added_tracker)
 	_added_tracker.errors.items.clear()
 
 
-func after_all():
+func after_each():
 	OS.remove_logger(_added_tracker)
+	_added_tracker = null
+
 
 func _divide_these(a, b):
 	return a / b
 
-func _assert_a_equals_b(a, b):
-	assert(a == b)
+func _assert_a_equals_b(a, b, extra_text):
+	assert(a == b, extra_text)
 
 
 func test_can_make_one():
 	var inst = GutErrorTracker.new()
 	assert_not_null(inst)
-
 
 
 func test_add_gut_error_adds_error_to_none():
@@ -33,7 +33,6 @@ func test_add_gut_error_adds_error_to_none():
 	assert_eq(err.code, 'this is an error', 'code')
 	assert_eq(err.error_type, GutErrorTracker.GUT_ERROR_TYPE, 'error_type')
 	assert_true(err.is_gut_error(), 'is_gut_error')
-	assert_eq(err.get_error_category(), GutErrorTracker.ERROR_CATEGORY.GUT, 'category')
 	assert_eq(inst.errors.items[GutErrorTracker.NO_TEST][0], err, 'tracked')
 	assert_false(err.is_engine_error(), 'is_engine_error')
 
@@ -45,7 +44,9 @@ func test_when_adding_gut_error_during_a_test_addtional_info_filled_in():
 
 	assert_eq(err.function, 'test_when_adding_gut_error_during_a_test_addtional_info_filled_in')
 	assert_eq(err.file, "res://test/unit/test_error_tracker.gd")
-	assert_eq(err.line, 44, 'this can break if this file changes')
+	# use between because the line number keeps changing as I work and a range
+	# is a good enough check that we are getting an expected value back.
+	assert_between(err.line, 40, 60, 'this can break if this file changes')
 	assert_eq(err.backtrace[0].function, 'test_when_adding_gut_error_during_a_test_addtional_info_filled_in')
 
 
@@ -63,8 +64,149 @@ func test_when_push_error_data():
 	assert_true(last_error.is_push_error())
 
 
-func test_assert_fails_data():
-	_assert_a_equals_b('foo', 'bar')
-	var last_error = _added_tracker.errors.items[GutErrorTracker.NO_TEST][0]
-	assert_not_null(last_error)
-	assert_true(last_error.is_assert())
+# func test_assert_fails_data():
+# 	_assert_a_equals_b('foo', 'bar', '')
+# 	var err = _added_tracker.errors.items[GutErrorTracker.NO_TEST][0]
+# 	assert_not_null(err)
+# 	assert_true(err.is_assert())
+# 	assert_eq(err.error_type, Logger.ERROR_TYPE_SCRIPT)
+
+
+# func test_assert_fails_data_with_assert_text():
+# 	_assert_a_equals_b('foo', 'bar', '')
+# 	var err = _added_tracker.errors.items[GutErrorTracker.NO_TEST][0]
+# 	assert_true(err.is_assert())
+
+
+func test_should_fail_true_for_gut_error():
+	_added_tracker.start_test('test')
+	_added_tracker.add_gut_error('this is a gut error')
+	assert_false(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_true_for_gut_error_when_flag_set():
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.start_test('test')
+	_added_tracker.add_gut_error('this is a gut error')
+	assert_true(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_false_for_engine_error():
+	_added_tracker.start_test('test')
+	_divide_these('word', 'sentence')
+	assert_false(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_true_for_engine_error_when_flag_set():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.start_test('test')
+	_divide_these('word', 'sentence')
+	assert_true(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_false_for_push_error():
+	_added_tracker.start_test('test')
+	push_error("this is a push error")
+	assert_false(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_true_for_push_error_when_flag_set():
+	_added_tracker.treat_push_error_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.start_test('test')
+	push_error("this is a push error")
+	assert_true(_added_tracker.should_test_fail_from_errors('test'))
+
+
+func test_should_fail_finds_second_error():
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.start_test('test')
+	push_error('this is the first error')
+	_added_tracker.add_gut_error('this is a gut error')
+	assert_true(_added_tracker.should_test_fail_from_errors('test'))
+
+func test_should_fail_does_not_mistake_engine_for_push():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.start_test('test')
+	push_error('this is the first error')
+	_added_tracker.add_gut_error('this is a gut error')
+	assert_false(_added_tracker.should_test_fail_from_errors('test'))
+
+
+# var fail_text_params = ParameterFactory.named_parameters(
+# 	['error_fail', 'gut_fail', 'push_fail', 'do_error', 'do_push', 'do_gut'],
+# 	[GutErrorTracker.TREAT_AS.FAILURE, GutErrorTracker.TREAT_AS.NOTHING, GutErrorTracker.TREAT_AS.NOTHING,
+# 		true, false, false]
+# )
+# func test_fail_text(params=use_parameters(fail_text_params)):
+# 	_added_tracker.treat_engine_errors_as = params.error_fail
+
+
+func test_fail_text_includes_nothing_by_default():
+	push_error('push nope')
+	_added_tracker.add_gut_error('gut error nope')
+	_divide_these('nope', 44)
+	assert_eq(_added_tracker.get_fail_text_for_errors(), '')
+
+
+func test_fail_text_includes_all_when_flags_set():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_push_error_as = GutErrorTracker.TREAT_AS.FAILURE
+
+	push_error('push nope')
+	_added_tracker.add_gut_error('gut error nope')
+	_divide_these('nope', 44)
+
+	var error_text = _added_tracker.get_fail_text_for_errors()
+
+	assert_string_contains(error_text, 'Invalid operands')
+	assert_string_contains(error_text, 'gut error nope')
+	assert_string_contains(error_text, 'push nope')
+
+
+func test_fail_text_can_exclude_push():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_push_error_as = GutErrorTracker.TREAT_AS.NOTHING
+
+	push_error('push nope')
+	_added_tracker.add_gut_error('gut error nope')
+	_divide_these('nope', 44)
+
+	var error_text = _added_tracker.get_fail_text_for_errors()
+
+	assert_string_contains(error_text, 'Invalid operands')
+	assert_string_contains(error_text, 'gut error nope')
+	assert_eq(error_text.find("push nope"), -1)
+
+
+func test_fail_text_can_exclude_engine_error():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.NOTHING
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_push_error_as = GutErrorTracker.TREAT_AS.FAILURE
+
+	push_error('push nope')
+	_added_tracker.add_gut_error('gut error nope')
+	_divide_these('nope', 44)
+
+	var error_text = _added_tracker.get_fail_text_for_errors()
+
+	assert_eq(error_text.find('Invalid operands'), -1)
+	assert_string_contains(error_text, 'gut error nope')
+	assert_string_contains(error_text, 'push nope')
+
+
+func test_fail_text_can_exclude_gut_error():
+	_added_tracker.treat_engine_errors_as = GutErrorTracker.TREAT_AS.FAILURE
+	_added_tracker.treat_gut_errors_as = GutErrorTracker.TREAT_AS.NOTHING
+	_added_tracker.treat_push_error_as = GutErrorTracker.TREAT_AS.FAILURE
+
+	push_error('push nope')
+	_added_tracker.add_gut_error('gut error nope')
+	_divide_these('nope', 44)
+
+	var error_text = _added_tracker.get_fail_text_for_errors()
+
+	assert_string_contains(error_text, 'Invalid operands')
+	assert_eq(error_text.find('gut error nope'), -1)
+	assert_string_contains(error_text, 'push nope')
