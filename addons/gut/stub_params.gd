dca15fbdc0521f79726391d26b38d026186f7006
namelessvoid <simon_kerler@web.de>
2025-09-22 19:53:48 +0200

    Extend abstract parent check to cover all stub scenarios

--- addons/gut/stub_params.gd
+++ addons/gut/stub_params.gd
@@ -82,13 +82,24 @@ func _is_parent_method_abstract():
 	if typeof(stub_target) != TYPE_OBJECT:
 		return false
 
-	var method_list: Array[Dictionary] = stub_target.get_script().get_script_method_list() \
-		.filter(func(meta: Dictionary):
-			return meta.name == stub_method
+	var method_list: Array
+	if stub_target.has_method('get_script_method_list'):
+		method_list = stub_target.get_script_method_list()
+	elif stub_target.has_method('get_script'):
+		method_list = stub_target.get_script().get_script_method_list()
+		
+	if method_list.is_empty():
+		return false
+
+	var stubbed_method_list: Array[Dictionary] = method_list.filter(func(meta: Dictionary):
+		return meta.name == stub_method
 	)
 
+	if stubbed_method_list.size() < 2:
+		return false
+
 	# Remove first item, because it is the child implementation.
-	var parent_method_flags = method_list[1].flags
+	var parent_method_flags = stubbed_method_list[1].flags
 	return (parent_method_flags & MethodFlags.METHOD_FLAG_VIRTUAL_REQUIRED) != 0
 
 # -------------------------
