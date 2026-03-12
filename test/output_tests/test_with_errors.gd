2a27e607707bdf09ead527641a439d42b016faca
Butch Wesley <butch.wesley@gmail.com>
2026-03-12 11:36:09 -0400

    improved error output
    Added improved error output.  I mostly used what was suggested in the issue.  Engine an push errors also include the the line number in the test script where the error was encountered.

--- test/output_tests/test_with_errors.gd
+++ test/output_tests/test_with_errors.gd
@@ -1,6 +1,63 @@
 extends GutTest
 
+func _generate_a_push_error():
+	push_error("hello push_error")
 
 func test_with_push_error():
 	push_error('this is a push error')
-	assert_true(true, 'passing assert')
+	pass_test('has push_error')
+
+
+func test_forced_engine_error() -> void:
+	OS.delay_usec(-10)
+	pass_test('has forced engine error')
+
+
+func test_gut_error():
+	gut.get_logger().error("manual error")
+	pass_test('has a gut error')
+
+
+func test_failing():
+	assert_false(true)
+
+
+func test_with_two_push_error():
+	push_error('this is a push error')
+	_generate_a_push_error()
+	pass_test('has push_error')
+
+
+func test_with_two_forced_engine_error() -> void:
+	OS.delay_usec(-10)
+	OS.delay_usec(-11)
+	pass_test('has forced engine error')
+
+
+func test_with_two_gut_error():
+	gut.get_logger().error("manual error")
+	gut.get_logger().error("error again")
+	pass_test('has a gut error')
+
+
+
+class TestInnerClassOutput:
+	extends GutTest
+
+	func test_with_push_error():
+		push_error('this is a push error')
+		pass_test('has push_error')
+
+
+	func test_forced_engine_error() -> void:
+		OS.delay_usec(-10)
+		pass_test('has forced engine error')
+
+
+	func test_gut_error():
+		gut.get_logger().error("manual error")
+		pass_test('has a gut error')
+
+
+	func test_failing():
+		assert_false(true)
--- test/unit/test_i803_engine_error.gd
+++
@@ -1,6 +0,0 @@
-extends GutTest
-
-
-func test_gut_error_reporting() -> void:
-	OS.delay_usec(-10)
-	assert_true(true)
